137761e76ba89f9fa9a26002e50f160fbaca1f1d
gabekole <gabekoleszar@gmail.com>
2025-07-09 10:18:43 -0400

    Add results route

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -2,4 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
+    route("results", "routes/results.tsx")
 ] satisfies RouteConfig;
